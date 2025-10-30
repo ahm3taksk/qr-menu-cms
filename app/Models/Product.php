b22d7caa12d9153c43bf9580322d3636ffb9a5ab
Ahmet Dursun <82242698+ahm3taksk@users.noreply.github.com>
2025-10-30 21:12:49 +0300

    Add Product & Category model and migrations

---
+++ app/Models/Product.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Spatie\Sluggable\HasSlug;
+use Spatie\Sluggable\SlugOptions;
+
+class Product extends Model
+{
+    use HasFactory, HasSlug;
+
+    protected $fillable = [
+        'category_id',
+        'name',
+        'slug',
+        'price',
+        'description',
+        'extra_fields',
+        'image',
+        'order'
+    ];
+
+    protected $casts = [
+        'extra_fields' => 'array',
+    ];
+
+    public function getSlugOptions(): SlugOptions
+    {
+        return SlugOptions::create()
+            ->generateSlugsFrom('name')
+            ->saveSlugsTo('slug')
+            ->slugsShouldBeNoLongerThan(255);
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+}
---
+++ database/migrations/2025_10_30_181210_create_products_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('category_id')->constrained()->cascadeOnDelete();
+            $table->string('name');
+            $table->string('slug')->unique();
+            $table->decimal('price', 8, 2);
+            $table->text('description')->nullable();
+            $table->json('extra_fields')->nullable(); // malzemeler, alerjenler vs
+            $table->string('image')->nullable();
+            $table->integer('order')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
